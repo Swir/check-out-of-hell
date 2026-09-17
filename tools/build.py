a822501f2831097d5720fbd5038cb18555b2cc33
Wojciech <swirjestem@hotmail.com>
2026-09-17 05:10:34 +0200

    Wire Overtime hazard assets into build

--- tools/build.py
+++ tools/build.py
@@ -8,6 +8,7 @@ from generate_combat_assets import generate_combat_assets
 from generate_vehicle_enemy_assets import generate_vehicle_enemy_assets
 from generate_regional_manager_assets import generate_regional_manager_assets
 from generate_presentation_assets import generate_presentation_assets
+from generate_overtime_assets import generate_overtime_assets
 
 ROOT = Path(__file__).resolve().parents[1]
 GAME = ROOT / "game"
@@ -63,6 +64,7 @@ generate_combat_assets(GAME)
 generate_vehicle_enemy_assets(GAME)
 generate_regional_manager_assets(GAME)
 generate_presentation_assets(GAME)
+generate_overtime_assets(GAME)
 pad_short_wavs(GAME / "sounds")
 
 built_maps = []
