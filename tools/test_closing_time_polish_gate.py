a9efd742b0eb58962fd7858e9c132577bfb1d95b
Wojciech <swirjestem@hotmail.com>
2026-09-20 08:49:25 +0200

    Protect collision-free Closing Time polish editor IDs

--- tools/test_closing_time_polish_gate.py
+++ tools/test_closing_time_polish_gate.py
@@ -14,7 +14,7 @@ ROUTE_SIGNS = {
     17161: ((-420.0, -215.0), "ClosingTimeLeftFuseSign", "LFSN", "LFSNA0.png"),
     17162: ((420.0, -215.0), "ClosingTimeRightFuseSign", "RFSN", "RFSNA0.png"),
     17163: ((-210.0, 285.0), "ClosingTimeStaffFuseSign", "SFSN", "SFSNA0.png"),
-    17164: ((-650.0, -75.0), "ClosingTimeOvertimeLaneSign", "OTSN", "OTSNA0.png"),
+    17170: ((-650.0, -75.0), "ClosingTimeOvertimeLaneSign", "OTSN", "OTSNA0.png"),
 }
 
 
